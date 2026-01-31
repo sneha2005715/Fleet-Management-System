74cb6b570f7201832e7e30ddfada0c112b9c3d63
snehanjali2005 <snehanjalirout2005@gmail.com>
2026-01-31 18:57:53 +0530

    Create rateLimiter.middleware.js

---
+++ rateLimiter.middleware.js
@@ -0,0 +1,17 @@
+const requests = {};
+
+export const rateLimiter = (req, res, next) => {
+  const ip = req.ip;
+  const now = Date.now();
+
+  if (!requests[ip]) requests[ip] = [];
+
+  requests[ip] = requests[ip].filter(time => now - time < 60000);
+
+  if (requests[ip].length >= 3) {
+    return res.status(429).json({ message: "Too many requests. Try later." });
+  }
+
+  requests[ip].push(now);
+  next();
+};
